504958014a594e622c3c7066382df73ad3ed189b
OllieM26 <ollie.madelin@gmail.com>
2022-07-15 19:05:03 +0100

    Second commit

--- contracts/GambleGamev4.sol
+++ contracts/GambleGamev4.sol
@@ -12,11 +12,11 @@ import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 contract GambleGamev4 is VRFConsumerBaseV2 {
     VRFCoordinatorV2Interface COORDINATOR;
     address public coordinatorAddress;
-    uint64 s_subscriptionId;
 
-    // Rinkeby coordinator and hash:
-    bytes32 keyHash; // = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
-    address vrfCoordinator; // = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    uint64 s_subscriptionId;
+    bytes32 keyHash;
+    address vrfCoordinator;
+    
     uint32 callbackGasLimit = 100000;
     uint16 requestConfirmations = 3;
     uint32 numWords = 1;
